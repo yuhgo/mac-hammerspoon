1c40c4c4d9e05879873f4e17ae5ac9bfb2d9d4b7
yuhgo <yuhgo090764@gmail.com>
2022-11-17 09:57:18 +0900

    ✨ vim風のカーソル移動に変更

--- init.lua
+++ init.lua
@@ -26,21 +26,21 @@ appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
 appsWatcher:start()
 
 -- カーソル移動
-remapKey({'right_ctrl'}, 'j', keyCode('left'))
-remapKey({'right_ctrl'}, 'k', keyCode('down'))
-remapKey({'right_ctrl'}, 'i', keyCode('up'))
+remapKey({'right_ctrl'}, 'h', keyCode('left'))
+remapKey({'right_ctrl'}, 'j', keyCode('down'))
+remapKey({'right_ctrl'}, 'k', keyCode('up'))
 remapKey({'right_ctrl'}, 'l', keyCode('right'))
-remapKey({'right_ctrl', 'shift'}, 'j', keyCode('left', {'shift'}))
-remapKey({'right_ctrl', 'shift'}, 'k', keyCode('down', {'shift'}))
-remapKey({'right_ctrl', 'shift'}, 'i', keyCode('up', {'shift'}))
+remapKey({'right_ctrl', 'shift'}, 'h', keyCode('left', {'shift'}))
+remapKey({'right_ctrl', 'shift'}, 'j', keyCode('down', {'shift'}))
+remapKey({'right_ctrl', 'shift'}, 'k', keyCode('up', {'shift'}))
 remapKey({'right_ctrl', 'shift'}, 'l', keyCode('right', {'shift'}))
-remapKey({'right_ctrl', 'cmd'}, 'j', keyCode('left', {'cmd'}))
-remapKey({'right_ctrl', 'cmd'}, 'k', keyCode('down', {'cmd'}))
-remapKey({'right_ctrl', 'cmd'}, 'i', keyCode('up', {'cmd'}))
+remapKey({'right_ctrl', 'cmd'}, 'h', keyCode('left', {'cmd'}))
+remapKey({'right_ctrl', 'cmd'}, 'j', keyCode('down', {'cmd'}))
+remapKey({'right_ctrl', 'cmd'}, 'k', keyCode('up', {'cmd'}))
 remapKey({'right_ctrl', 'cmd'}, 'l', keyCode('right', {'cmd'}))
-remapKey({'right_ctrl', 'shift', 'cmd'}, 'j', keyCode('left', {'shift', 'cmd'}))
-remapKey({'right_ctrl', 'shift', 'cmd'}, 'k', keyCode('down', {'shift', 'cmd'}))
-remapKey({'right_ctrl', 'shift', 'cmd'}, 'i', keyCode('up', {'shift', 'cmd'}))
+remapKey({'right_ctrl', 'shift', 'cmd'}, 'h', keyCode('left', {'shift', 'cmd'}))
+remapKey({'right_ctrl', 'shift', 'cmd'}, 'j', keyCode('down', {'shift', 'cmd'}))
+remapKey({'right_ctrl', 'shift', 'cmd'}, 'k', keyCode('up', {'shift', 'cmd'}))
 remapKey({'right_ctrl', 'shift', 'cmd'}, 'l', keyCode('right', {'shift', 'cmd'}))
 
 -- Home/End
@@ -51,7 +51,7 @@ remapKey({'right_ctrl', 'shift'}, 'f', keyCode('end', {'shift'}))
 
 -- Enter/Backspace/Delete
 remapKey({'right_ctrl'}, 'n', keyCode('return'))
-remapKey({'right_ctrl'}, 'h', keyCode('delete'))
+remapKey({'right_ctrl'}, 'b', keyCode('delete'))
 remapKey({'right_ctrl'}, ';', keyCode('forwarddelete'))
 
 -- 記号など
